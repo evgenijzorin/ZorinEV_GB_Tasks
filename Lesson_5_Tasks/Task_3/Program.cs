5b49a3b9c6ff3bf8180506c706459efe08dcbae6
EvgenijZorin <evgenijzorin@bk.ru>
2021-05-17 17:27:16 +0300

    created tasks for lesson 5

---
+++ Lesson_5_Tasks/Task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите произвольный набор чисел, от 0 до 255, используя пробел в качестве разделителя:");
+            string userText = Console.ReadLine();
+            // Перевести строку в массив
+            string[] userTextArray = userText.Split(' ');
+            // перевести строковый массив в массив byte массив
+            byte[] userNumArray = new byte[userTextArray.Length];// создать массив
+            for(int i = 0; i < userTextArray.Length; i++)
+            {
+                userNumArray[i] = Convert.ToByte(userTextArray[i]);
+            }
+            File.WriteAllBytes("user_numbers.bin", userNumArray); // сохранить файл
+            Console.WriteLine("Файл user_numbers.bin с числами, которые вы ввели, сохранен");
+            // File.ReadAllBytes("user_numbers.bin");            
+        }
+    }
+}
